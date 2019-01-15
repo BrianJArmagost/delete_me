b3cc85eb5694493c53f969740bd8ca420e6c3f2c
BrianJArmagost <BArmagost@DataRecognitionCorp.com>
2019-01-15 17:19:58 -0600

    guild-001

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'page1',
+    component: Page1Component,
+  },
+  {
+    path: 'page2',
+    component: Page2Component,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
